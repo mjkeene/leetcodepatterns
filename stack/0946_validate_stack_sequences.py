c159ed52fff2e7e8c2dcdab08e79a091d68551e8
Michael Keene <mikekeene21@gmail.com>
2025-01-17 14:00:18 -0700

    [Stack] Adding 0946 validate stack sequences

---
+++ stack/0946_validate_stack_sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        i = 0
+        stack = []
+
+        for n in pushed:
+            stack.append(n)
+            while i < len(pushed) and stack and popped[i] == stack[-1]:
+                stack.pop()
+                i += 1
+
+        return not stack
