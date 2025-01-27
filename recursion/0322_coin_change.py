10a6e8ad6449e30a21a2e16fe92210d5283861b1
Michael Keene <mikekeene21@gmail.com>
2025-01-26 21:37:55 -0700

    [Dynamic Programming] Revisiting 0322 coin change

--- recursion/0322_coin_change.py
+++ recursion/0322_coin_change.py
@@ -1,12 +1,18 @@
 class Solution:
-    def coinChange(self, coins, amount):
-        dp = [amount + 1] * (amount + 1)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # note that greedy approach does not work for this
+        # sort so that we can break if amount - coin is negative
+        coins.sort()
+
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
-        for a in range(1, amount + 1):
+        for amount in range(1, amount + 1):
             for coin in coins:
-                if a - coin >= 0:
-                    # possibly found solution for dp
-                    dp[a] = min(dp[a], 1 + dp[a - coin])
+                diff = amount - coin
+                if diff < 0:
+                    break
+                dp[amount] = min(dp[amount], dp[diff] + 1)
 
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        return dp[amount] if dp[amount] != float('inf') else -1
+    
