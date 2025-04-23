9b8b1f705b211c8f1fcf32429ff4068694826dc5
Mike <mikekeene21@gmail.com>
2025-04-23 00:59:51 -0600

    [String] Add LC 0007 reverse integer

---
+++ string/0007_reverse_integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x *= sign
+
+        res = int(str(x)[::-1])
+        res *= sign
+
+        if res > 2**31 - 1 or res < -2**31:
+            return 0
+
+        return res
