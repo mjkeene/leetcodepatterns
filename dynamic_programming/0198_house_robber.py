0885de9dc64e6552219ac17598de1424ab1f8535
Mike <mikekeene21@gmail.com>
2025-04-24 13:51:03 -0600

    [Dynamic Programming] Add 0198 house robber

---
+++ dynamic_programming/0198_house_robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums)
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            val = nums[i] + dp[i-2]
+            best = max(val, dp[i-1])
+            dp[i] = best
+
+        return dp[-1]
