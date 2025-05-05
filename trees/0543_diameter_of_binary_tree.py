b1299fea8beed5f3574dfee4203f3e8a473d129c
mjkeene <mikekeene21@gmail.com>
2025-05-04 23:39:43 -0400

    [Tree] Add 0543 diameter of binary tree

--- trees/0543_diameter_of_binary_tree.py
+++ trees/0543_diameter_of_binary_tree.py
@@ -4,20 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res = [0]
+        def dfs(node):
+            if not node:
+                return 0
 
-        def dfs(root):
-            if not root:
-                return -1
-
-            left = dfs(root.left)
-            right = dfs(root.right)
-            res[0] = max(res[0], 2 + left + right)
+            nonlocal res
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res = max(res, left + right)
 
             return 1 + max(left, right)
 
+        res = 0
         dfs(root)
-        return res[0]
+        return res
