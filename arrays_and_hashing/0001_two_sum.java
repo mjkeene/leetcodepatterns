5ebe42c645df46d703489d6f6c17a8ca28658e72
mjkeene <mikekeene21@gmail.com>
2025-05-27 16:23:44 -0600

    [Java] Adding two sum in Java, HashMap

--- arrays_and_hashing/0001_two_sum.java
+++ arrays_and_hashing/0001_two_sum.java
@@ -1,12 +1,30 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer, Integer> mapping = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (mapping.containsKey(diff)) {
+                return new int[] { mapping.get(diff), i };
             }
+
+            mapping.put(nums[i], i);
         }
-        return new int[]{};
+
+        return new int[0];
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
