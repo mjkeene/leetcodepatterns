78cad29d67d9223b6487852a401e9308df579d42
mjkeene <mikekeene21@gmail.com>
2025-07-21 15:00:22 -0600

    [Array/Hashing] Add 0049 group anagrams

--- arrays_and_hashing/0049_group_anagrams.py
+++ arrays_and_hashing/0049_group_anagrams.py
@@ -1,18 +1,16 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Create mapping, iterate over strs, sort the word
-        # If that key (sorted word) already exists in dict, append the original word
-        # Else, initialize the key/val pair with sorted word, empty array
-        # Append that unsorted word to that key's array
-
-        mapping = {}
-        for s in strs:
-            sorted_s = ''.join(sorted(s))
-            mapping.setdefault(sorted_s, [])
-            mapping[sorted_s].append(s)
-
+        mapping = defaultdict(list)
         res = []
-        for k, v in mapping.items():
-            res.append(v)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            mapping[sorted_word].append(word)
+
+        for key, value in mapping.items():
+            res.append(value)
 
         return res
+    
