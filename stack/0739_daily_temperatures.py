af67abd4cc254012c550f1e302a34eeb4f6a7b95
mjkeene <mikekeene21@gmail.com>
2025-08-14 10:34:49 -0600

    [Stack] Revisit 0739 daily temperatures

--- stack/0739_daily_temperatures.py
+++ stack/0739_daily_temperatures.py
@@ -3,21 +3,12 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []
+
         for idx, temp in enumerate(temperatures):
             while stack and stack[-1][1] < temp:
-                curr_item = stack.pop()
-                curr_index = curr_item[0]
-                res[curr_index] = idx - curr_index
+                popped = stack.pop()
+                num_days = idx - popped[0]
+                res[popped[0]] = num_days
             stack.append((idx, temp))
-        return res
 
-        # for idx, temp in enumerate(temperatures):
-        #     if not stack or temp < stack[-1][0]:
-        #         stack.append((temp, idx))
-        #     else:
-        #         while stack and stack[-1][0] < temp:
-        #             days = idx - stack[-1][1]
-        #             res[stack[-1][1]] = days
-        #             stack.pop()
-        #         stack.append((temp, idx))
-        # return res
+        return res
